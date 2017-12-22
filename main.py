eec77813446d71636f9616f9ddcce69ccc3c92f2
kathiecham <kathyswg@hotmail.com>
2017-12-21 20:03:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 275px;
+                height: 200px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 250px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+        <form id="input_form" method="POST">
+            <label> Rotate by: 
+            <input type="text" name="rot" value="0" /> </label>
+            <textarea name="text">{0}</textarea> <br>
+            <input type="submit" value="Submit Query"/>
+        <form/>
+    </body>
+</html>
+"""
+@app.route("/")
+def index():
+    return form.format ("")
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    text = request.form ['text']
+    rot = request.form ['rot']
+    rot = int(rot)
+    rotated = rotate_string(text,rot)
+    return form.format(rotated)
+
+app.run()
